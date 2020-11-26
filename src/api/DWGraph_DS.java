c92f8e595b2fbda36edc0f5c40b18c7cbc2e3a65
Dvir Avrahami <dviravr@gmail.com>
2020-11-26 12:49:29 +0200

    remove node

--- src/api/DWGraph_DS.java
+++ src/api/DWGraph_DS.java
@@ -1,19 +1,19 @@
 package api;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
+import java.util.*;
 
 public class DWGraph_DS implements directed_weighted_graph {
 
    private final HashMap<Integer, node_data> _graphNodes;
    private final HashMap<Integer, HashMap<Integer, edge_data>> _graphEdges;
+   private final HashMap<Integer, HashSet<Integer>> _destNi;
    private int modeCount = 0;
    private int edgeSize = 0;
 
    public DWGraph_DS() {
       _graphNodes = new HashMap<>();
       _graphEdges = new HashMap<>();
+      _destNi = new HashMap<>();
    }
 
    public DWGraph_DS(directed_weighted_graph g) {
@@ -40,6 +40,7 @@ public class DWGraph_DS implements directed_weighted_graph {
       if (hasNode(n.getKey())) {
          _graphNodes.put(n.getKey(), n);
          _graphEdges.put(n.getKey(), new HashMap<>());
+         _destNi.put(n.getKey(), new HashSet<>());
          modeCount++;
       }
    }
@@ -47,8 +48,11 @@ public class DWGraph_DS implements directed_weighted_graph {
    @Override
    public void connect(int src, int dest, double w) {
       if (hasNode(src) && hasNode(dest) && w >= 0) { // todo: maybe just w > 0
+         if (!hasEdge(src, dest)) {
+            edgeSize++;
+         }
+         _destNi.get(dest).add(src);
          _graphEdges.get(src).put(dest, new EdgeData(src, dest, w));
-         edgeSize++;
          modeCount++;
       }
    }
@@ -68,7 +72,22 @@ public class DWGraph_DS implements directed_weighted_graph {
 
    @Override
    public node_data removeNode(int key) {
-      return null;
+      node_data node = getNode(key);
+      if (node != null) {
+//         if the node exist in the graph and removing all his edges
+//         first, removing all of the edges that the node is the dest node
+         ArrayList<HashSet<Integer>> destNis = new ArrayList<>(_destNi.values());
+//         destNis have just one element - set of the sources
+         for (int ni : destNis.get(0)) {
+            removeEdge(ni, key);
+         }
+//         after he isn't the dest of any node removing him from the graph
+         edgeSize -= _graphEdges.get(key).size();
+         _graphEdges.remove(key);
+         _graphNodes.remove(key);
+         modeCount++;
+      }
+      return node;
    }
 
    @Override
@@ -76,6 +95,7 @@ public class DWGraph_DS implements directed_weighted_graph {
       if (hasEdge(src, dest)) {
          edgeSize--;
          modeCount++;
+         _destNi.get(dest).remove(src);
          return _graphEdges.get(src).remove(dest);
       }
       return null;
